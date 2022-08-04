1d9b1c9f16ec71752d07a72fc9d6b5faef768c77
Yuta Katayama <yutapilafu@gmail.com>
2022-08-04 21:19:43 +0900

    babel/preset-envのnodeを4に設定

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	// presets: [['@babel/preset-env', { targets: { chrome: 90 } }]]
-	presets: [['@babel/preset-env', { targets: { node: 3 } }]]
+	presets: [['@babel/preset-env', { targets: { node: 4 } }]]
 };
--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@ var __webpack_exports__ = {};
   !*** ./src/index.js ***!
   \**********************/
 var show = function show(text) {
-  return "".concat(text, "\u3001\u3055\u3088\u3046\u306A\u3089");
+  return `${text}、さようなら`;
 };
 
 var text = show('こんにちは');
